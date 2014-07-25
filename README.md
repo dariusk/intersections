111d0d2fbcbf76b32821c24785d751f943b5992e
Kazemi, Darius <dkazemi@akamai.com>
2014-07-24 20:17:08 -0400

    First

---
+++ README.md
@@ -0,0 +1,7 @@
+# intersections (Intersections)
+
+The best project ever.
+
+## License
+Copyright (c) 2014 Kazemi, Darius  
+Licensed under the MIT license.
---
+++ index.js
@@ -0,0 +1,216 @@
+var request = require('request');
+var _ = require('underscore');
+_.mixin( require('underscore.deferred') );
+var inflection = require('inflection');
+var Twit = require('twit');
+var T = new Twit(require('./config.js'));
+var wordfilter = require('wordfilter');
+var wordnikKey = require('./permissions.js').key;
+var google = require('google');
+var wf = require('word-freq');
+
+var DEBUG = false;
+
+var WIDTH = 800,
+    HEIGHT = 800;
+var Canvas = require('canvas'),
+    Image = Canvas.Image,
+    canvas = new Canvas(WIDTH, HEIGHT),
+    ctx = canvas.getContext('2d');
+
+ctx.circle = function(x, y, r) {
+  this.arc(x, y, r, 0, Math.PI*2, true);
+}
+
+ctx.makeCircle = function(x, y, r, txt, modx, mody) {
+  var modx = modx || 0;
+  var mody = mody || 0;
+  var hue = Math.random();
+  this.beginPath();
+  this.circle(x, y, r);
+  this.fillStyle = hsla(hue, 1, 0.5, 0.25);
+  this.fill();
+  this.font = '30px Impact';
+  this.fillStyle = hsla(hue, 1, 0.2, 1);
+  var mt = this.measureText(txt);
+  this.fillText(txt.toUpperCase(), x - mt.width/2 + WIDTH*modx, y + HEIGHT*mody);
+}
+
+Array.prototype.pick = function() {
+  return this[Math.floor(Math.random()*this.length)];
+};
+
+Array.prototype.pickRemove = function() {
+  var index = Math.floor(Math.random()*this.length);
+  return this.splice(index,1)[0];
+};
+
+function getThreeNouns() {
+  var dfd = new _.Deferred();
+
+  var url = 'http://api.wordnik.com/v4/words.json/randomWords?minCorpusCount=5000&minDictionaryCount=5&excludePartOfSpeech=proper-noun,proper-noun-plural,proper-noun-posessive,suffix,family-name,idiom,affix&hasDictionaryDef=true&includePartOfSpeech=noun&limit=1000&maxLength=22&api_key='+wordnikKey;
+  request(url, function(err, response, body) {
+    var words = JSON.parse(body);
+    words = _.pluck(words, 'word');
+    dfd.resolve([words.pickRemove(), words.pickRemove(), words.pickRemove()]);
+  });
+  return dfd.promise();
+}
+
+function generate() {
+  var dfd = new _.Deferred();
+
+  getThreeNouns().done(function(res) {
+    console.log(res);
+
+    var A = res[0] || 'dog',
+        B = res[1] || 'tree',
+        C = res[2] || 'pavement',
+        mt;
+
+    _.when(
+        intersect(A, B),
+        intersect(A, C),
+        intersect(C, B),
+        intersect(C, B, A)
+      ).done(function(AB, AC, CB, CBA) {
+
+      ctx.fillStyle = "#ddd";
+      ctx.fillRect(0, 0, WIDTH, HEIGHT);
+      ctx.makeCircle(WIDTH*0.35, HEIGHT*0.65, HEIGHT*0.3, A, -0.15, 0.1);
+      ctx.makeCircle(WIDTH*0.65, HEIGHT*0.65, HEIGHT*0.3, B, 0.15, 0.1);
+      ctx.makeCircle(WIDTH*0.5, HEIGHT*0.35, HEIGHT*0.3, C, 0, -0.15);
+
+      ctx.font = '30px Impact';
+      ctx.fillStyle = 'black';
+      mt = ctx.measureText(AB);
+      ctx.fillText(AB.toUpperCase(), WIDTH/2 - mt.width/2, HEIGHT*0.75);
+      mt = ctx.measureText(AC);
+      ctx.fillText(AC.toUpperCase(), WIDTH*0.31 - mt.width/2, HEIGHT*0.46);
+      mt = ctx.measureText(CB);
+      ctx.fillText(CB.toUpperCase(), WIDTH*0.69 - mt.width/2, HEIGHT*0.46);
+      mt = ctx.measureText(CBA);
+      ctx.fillText(CBA.toUpperCase(), WIDTH*0.5 - mt.width/2, HEIGHT*0.55);
+
+
+      makePng(canvas);
+      dfd.resolve(AB);
+    });
+  });
+
+  return dfd.promise();
+}
+
+function intersect(A, B, C) {
+  var dfd = new _.Deferred();
+  var result = '';
+  
+  var search = C ? A + ' ' + B + ' ' + C : A + ' ' + B;
+  console.log(search);
+
+  if (DEBUG) {
+    result = 'thing';
+    console.log('debugging');
+    dfd.resolve(result);
+    return dfd.promise();
+  }
+
+  google.resultsPerPage = 10;
+  var nextCounter = 0;
+
+  google(search, function(err, next, links){
+    if (err) console.error(err);
+
+    for (var i = 0; i < links.length; ++i) {
+      result += links[i].title + ' ' + links[i].description + ' ';
+    }
+
+    if (nextCounter < 4) {
+      nextCounter += 1;
+      if (next) next();
+    }
+    else {
+      var frequency = wf.freq(result, true, false);
+      var stemA = wf.stem(A)[0];
+      var stemB = wf.stem(B)[0];
+      var stemC = C ? wf.stem(C)[0] : '';
+      C = C || '';
+      //console.log(stemA, stemB);
+      result = _.chain(frequency)
+        .pairs()
+        // Filter out all words that share a stem with the input words
+        .reject(function(el) {
+          var stemEl = wf.stem(el[0])[0];
+          return el.length < 3 || stemEl === stemA || stemEl === stemB || stemEl === stemC ||
+                 stemEl.indexOf(A.substr(0,5)) > -1 ||
+                 stemEl.indexOf(B.substr(0,5)) > -1 ||
+                 stemEl.indexOf(C.substr(0,5)) > -1
+        })
+        // Now we find the maximum frequency word
+        .max(function(el) {
+          return el[1];
+        })
+        .value()[0];
+      dfd.resolve(result);
+    }
+
+  });
+  return dfd.promise();
+}
+
+function tweet() {
+  generate().then(function(myTweet) {
+    if (!wordfilter.blacklisted(myTweet)) {
+      console.log(myTweet);
+      /*
+      T.post('statuses/update', { status: myTweet }, function(err, reply) {
+        if (err) {
+          console.log('error:', err);
+        }
+        else {
+          console.log('reply:', reply);
+        }
+      });
+      */
+    }
+  });
+}
+
+function makePng(canvas) {
+  var fs = require('fs'),
+      out = fs.createWriteStream(__dirname + '/out.png'),
+      stream = canvas.pngStream();
+
+  stream.on('data', function(chunk){
+    out.write(chunk);
+  });
+
+  stream.on('end', function(){
+    console.log('saved png');
+  });
+}
+
+function hsla(h, s, l, a){
+  var r, g, b;
+  if(s == 0){
+    r = g = b = l; // achromatic
+  }
+  else {
+    function hue2rgb(p, q, t){
+      if(t < 0) t += 1;
+      if(t > 1) t -= 1;
+      if(t < 1/6) return p + (q - p) * 6 * t;
+      if(t < 1/2) return q;
+      if(t < 2/3) return p + (q - p) * (2/3 - t) * 6;
+      return p;
+    }
+    var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+    var p = 2 * l - q;
+    r = hue2rgb(p, q, h + 1/3);
+    g = hue2rgb(p, q, h);
+    b = hue2rgb(p, q, h - 1/3);
+  }
+  return 'rgba(' + Math.round(r * 255) + ', ' + Math.round(g * 255) + ', ' + Math.round(b * 255) + ', ' + a + ')';
+}
+// Tweet once on initialization
+tweet();
